585a31c1444c32dd93c861e5f985aa60b7ef91f3
crazytaj <43589239+crazytaj@users.noreply.github.com>
2018-10-29 17:20:10 -0500

    Update blog/dbconnect.php

--- blog/dbconnect.php
+++ blog/dbconnect.php
@@ -2,7 +2,7 @@
 $db_host = 'localhost';
 $db_name = 'stuco';
 $db_user = 'root';
-$db_pass = '#XFCE4life!';
+$db_pass = '';
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
