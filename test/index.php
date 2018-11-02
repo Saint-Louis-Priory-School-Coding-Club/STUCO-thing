316fb3adbabf150b29a3d476783c4a62703942f2
crazytaj <43589239+crazytaj@users.noreply.github.com>
2018-11-01 21:57:13 -0500

    cleaning up un-needed files

--- test/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../blog/dbconnect.php';
-$sql = $conn->query("SELECT COUNT(*) FROM numbers");
-$res = mysqli_fetch_assoc($sql);
-echo $res["COUNT(*)"];
-$str = "test's";
-echo mysqli_real_escape_string($conn,$str);
-?>
