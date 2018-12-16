6b0422bde4af324ffe54fc2711f51ed5b03813b4
RoboRebels1329 <crossei@stlprioryschool.org>
2018-12-16 10:37:58 -0600

    Spell fixes

--- blog/content.php
+++ blog/content.php
@@ -7,7 +7,7 @@ Add
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Add Announcment</h5>
+        <h5 class="modal-title">Add Announcement</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
