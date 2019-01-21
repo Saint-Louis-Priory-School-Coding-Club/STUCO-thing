e2e198ff7e43f376e79a299b3c9715680f789ff0
crazytaj <43589239+crazytaj@users.noreply.github.com>
2019-01-21 10:33:26 -0600

    admin dashboard

--- Controllers/user.php
+++ Controllers/user.php
@@ -14,10 +14,85 @@ class User extends Controller {
 
     }
 
-    function dashboard() {
-        require_once './Models/tasks_model.php';
-        //$model = new Index_Model;
-        $this->view->render('./Views/user-dashboard', FALSE);
+
+    public function user($parameters = NULL) {
+        //put in parameters here
+        $accepted_params = [];
+        $params = array();
+        for ($i = 0; $i < sizeof($accepted_params); $i++) {
+            if (!isset($parameters[$i])) {
+                break;
+            }
+            $arraytopush = array(
+                $accepted_params[$i]    => $parameters[$i]
+            );
+            $params = array_merge($params, $arraytopush);
+        }
+        unset($params['']);
+        require_once './Models/index_model.php';
+        $model = new Index_Model;
+        //loads the page with the header and footer already included, set to FALSE to control the full page
+        $this->view->render('./Views/help',TRUE,$params);
+    }
+    function dashboard($pages = NULL) {
+        $f1_pages = ['pages','stuco'];
+        $f2_pages = array(
+            'pages0'     => 'add',
+            'pages1'     => 'edit'
+        );
+        $f3_params = array(
+            'pages::edit'    => 'page_id'
+        );
+        $pagetoload = '';
+        $pass_params = [];
+        $f3 = FALSE;
+        $f2 = FALSE;
+        $f1 = FALSE;
+        $perror = FALSE;
+        if ($pages !== NULL) {
+            for ($i = 0; $i < sizeof($pages); $i++) {
+                //var_dump($pages[$i]);
+                if (!isset($pages[$i])) break;
+                if (in_array($pages[$i],$f1_pages) && $f1 === FALSE) {
+                    $pagetoload = $pages[$i];
+                    $f1 = TRUE;
+                } elseif (in_array($pages[$i],$f2_pages) && $f2 === FALSE && $f1 === TRUE) {
+                    $pagetoload .= '/' . $pages[$i];
+                    $f2 = TRUE;
+                } elseif ($pagetoload !== NULL && sizeof($pages) > 2 && $f2 === TRUE && $f1 === TRUE) {
+                    $f3 = TRUE;
+                    $param_key = explode('/', $pagetoload);
+                    $param_key = $param_key[0] . '::' . $param_key[1];
+                    if (!isset($f3_params[$param_key])) break;
+                    $arraytopush = array(
+                        $f3_params[$param_key]        => $pages[$i]
+                    );
+                    $pass_params = array_merge($pass_params, $arraytopush);
+                } else {
+                    $perror = TRUE;
+                }
+            }
+            //var_dump($f2);
+            require_once './Models/pages_model.php';
+            if ($perror) $this->view->e404();
+            if ($f3) {
+                $loadpage = explode('/',$pagetoload);
+                $loadpage = 'dashboard-'.$loadpage[0].'-'.$loadpage[1];
+                $this->view->render('./Views/'.$loadpage,FALSE,$pass_params);
+            } elseif ($f2) {
+                $loadpage = explode('/',$pagetoload);
+                $loadpage = 'dashboard-'.$loadpage[0].'-'.$loadpage[1];
+                $this->view->render('./Views/'.$loadpage,FALSE);
+            } elseif ($f1) {
+                $this->view->render('./Views/dashboard-'.$pagetoload,FALSE);
+            }
+
+
+        } else {
+            require_once './Models/tasks_model.php';
+            //$model = new Index_Model;
+            $this->view->render('./Views/user-dashboard', FALSE);
+        }
     }
 
     function login() {
--- Views/user-dashboard.php
+++ Views/user-dashboard.php
@@ -1,143 +1,69 @@
-<?php
-if (!isset($userRow)) {
-    header('Location: ' . '/user/login');
-}
-$functions = new UserData;
-$userData = $functions->use_userdata($userRow['userdata'], $userRow['password']);
-if (isset($_GET['name'])) {
-    $defval = $_GET['name'];
-} else {
-    $defval = NULL;
-}
-if (isset($_POST['stucoshift'])) {
-    $idtochange = $_POST['stucoshift'];
-    $namer = $_POST['stucoshifter'];
-    $stuco = $_POST['stuco'];
-    $userData['prevstuco'] = 1;
-    $userDatas = $functions->store_userdata($userData);
-    $conn->query("UPDATE users SET stuco=".$stuco.", userdata='".$userDatas."' WHERE id= ".$idtochange."");
-    $_SESSION['stucoadd'] = $namer;
-    $_SESSION['stucoadds'] = $stuco;
-}
-require_once 'header.php';
-?>
-<title>Dashboard</title>
-<script>
-            function alertm(message, type) {
-                document.getElementById('alerts').style.display = 'block';
-                document.getElementById('messages').innerHTML = message;
-                document.getElementById('spacer').style.display = 'none';
-                switch (type) {
-                    case 'error':
-                        document.getElementById('alerts').style.backgroundColor = 'red';
-                    break;
-                    case 'success':
-                        document.getElementById('alerts').style.backgroundColor = 'lightgreen';
-                    break;
-                    default:
-                        document.getElementById('alerts').style.backgroundColor = 'yellow';
-                    break;
-                }
-
-            }
-
-            function closem() {
-                document.getElementById('alerts').style.display = 'none';
-                document.getElementById('spacer').style.display = 'block';
-            }
-        </script>
-
-        <div id="alerts" class="alert" style="display:none">
-            <button class="float-right closer" type="button" onclick="closem()">X</button>
-            <h1 class="messages" id="messages"></h1>
-        </div>
-        <div class="alert-spacer" id="spacer"></div>
-        <?php
-            if (isset($_SESSION['stucoadd'])) {
-                $names = $_SESSION['stucoadd'];
-                $stuco = $_SESSION['stucoadds'];
-                if ($stuco == 1) {
-                    echo "
-                    <script>
-                        alertm('$names is now in STUCO!', 'success');
-                    </script>
-                    ";
-                } else {
-                    echo "
-                    <script>
-                        alertm('$names is not in STUCO!', 'error');
-                    </script>
-                    ";
-                }
-                unset($_SESSION['stucoadds']);
-                unset($_SESSION['stucoadd']);
-            }
-        ?>
-        <div class="body">
-            <div class="left">
-                <?php
-                    if($userRow['admin'] == 1) {
-                        ?>
-                            <h2>Site Members:</h2>
-                            <div>
-                            <form method="GET">
-                                <input type="text" name="name" placeholder="Name" value="<?php echo $defval?>" required>
-                            </form><br>
-                        <?php
-                        if (isset($_GET['name'])) {
-                            $name = $_GET['name'];
-                            $sql = $conn->query("SELECT * FROM users WHERE flname LIKE '%".$name."%'");
-                            if ($sql !== FALSE) {
-                                foreach ($sql as $ind) {
-                                    $iname = $ind['flname'];
-                                    $id = $ind['id'];
-                                    if ($ind['stuco'] == 1) {
-                                        $message = $iname . ' is not in STUCO!'
-                                        ?>
-                                        <form method="POST">
-                                            <input type="hidden" value="<?php echo $id?>" name="stucoshift">                                        <input type="hidden" value="<?php echo $id?>" name="stucoshift">
-                                            <input type="hidden" value="<?php echo $iname?>" name="stucoshifter">
-                                            <input type="hidden" value="0" name="stuco">
-                                            <button class="btn btn-danger float-right" type="submit" onclick="alertm('<?php echo $message?>', 'error')">Kick from Stuco</button>
-                                        </form>
-                                        <p><?php echo $iname?></p>
-                                        <?php
-                                    } else {
-                                        $message = $iname . ' is now in STUCO!'
-                                        ?>
-                                        <form method="POST">
-                                            <input type="hidden" value="<?php echo $id?>" name="stucoshift">                                        <input type="hidden" value="<?php echo $id?>" name="stucoshift">
-                                            <input type="hidden" value="<?php echo $iname?>" name="stucoshifter">
-                                            <input type="hidden" value="1" name="stuco">
-                                            <button class="btn btn-primary float-right" type="submit" onclick="alertm('<?php echo $message?>', 'success')">Make Stuco</button>
-                                        </form>
-                                        <p><?php echo $iname?></p>
-                                        <?php
-                                    }
-                                }
-                            } else {
-                                echo '<p style="color:red"><strong>No users with that name exist</strong></p>';
-                            }
-                        }
-                    } elseif ($userRow['stuco'] == 1){
-                        
-                    } else {
-
-                    }
-                ?>
+<?php include 'dashboard-header.php'?>
+      <div id="content-wrapper">
+        <div class="container-fluid">
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="#">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Overview</li>
+          </ol>
+          <!-- End Breadcrumbs-->
+          
+          <div class='row'>
+          <?php
+          $sql = $conn->query("SELECT * FROM adminspanel") or die($conn->error);
+          $cardHeaders = array();
+          foreach ($sql as $ind) {
+            $color = $ind['color'];
+            $arraytopush = array(
+              "color"   => $ind['color'],
+              "icon"    => $ind['icon'],
+              "message" => $ind['message'],
+              "unread"  => "0",
+              "link"    => $ind['link']
+            );
+            array_push($cardHeaders, $arraytopush);
+          }
+            foreach ($cardHeaders as $key => $ind) {
+              $bgColor = $ind['color'];
+              $faIcon = $ind['icon'];
+              $number = $ind['unread'];
+              $message = $ind['message'];
+              $link = $ind['link'];
+              echo '
+              <div class="col-xl-3 col-sm-6 mb-3">
+              <div class="card text-white bg-'.$bgColor.' o-hidden h-100">
+                <div class="card-body">
+                  <div class="card-body-icon">
+                    <i class="'.$faIcon.'"></i>
+                  </div>
+                  <div class="mr-5">'.$number . ' ' . $message .'</div>
                 </div>
+                <a class="card-footer text-white clearfix small z-1" href="'.$link.'">
+                  <span class="float-left">View Details</span>
+                  <span class="float-right">
+                    <i class="fas fa-angle-right"></i>
+                  </span>
+                </a>
+              </div>
             </div>
-            <div class="main">
-                <?php
-                    foreach ($userData as $key => $val) {
-                        if ($val == $userData['key']);
-                        else {
-                        echo '
-                        <p><strong>'.$key.': </strong>'.$val.'</p>
-                        ';
-                        }
-                    }
-                ?>
+            ';
+            }
+          ?>
+
+          <!--   Area Chart
+          <div class="card mb-3">
+            <div class="card-header">
+                <i class="fas fa-chart-area"></i>
+                Area Chart Example</div>
+              <div class="card-body">
+                <canvas id="myAreaChart" width="100%" height="30"></canvas>
+              </div>
+              <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
             </div>
-        </div>
-<?php require_once 'footer.php';?>
+          </div>
+          -->
+
+
+        <?php include 'dashboard-footer.php'?>
