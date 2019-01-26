765305a84e78949d7297660902826425ce1a504d
CedricBruges <cbruges@stlprioryschool.org>
2019-01-26 16:26:40 -0600

    Update footer.php

--- Views/footer.php
+++ Views/footer.php
@@ -1,4 +1,4 @@
 </body>
-  <p style="text-align: center; font-size: 13px;"> This website was created by the Priory Coding Club. <br> © Coding Club 2019 </p>
+  <p style="text-align: center; font-size: 13px;"> This website was created by the Priory Coding Club. <br> Copyright © Coding Club 2019 </p>
 
 </html>
