aee3fc57c5170eadca3c12d1d1c8018c4575d443
Hexbugman213 <minecrafthex213@gmail.com>
2019-02-02 15:10:29 -0600

    index is now nice

--- Views/index.php
+++ Views/index.php
@@ -8,44 +8,87 @@
 } ?>
 <head>
   <style>
-  h1 {
-    color:red;
-  }
-  h2{
-    color:red;
-  }
+      h1 {
+        color:red;
+      }
+      h2{
+        color:red;
+      }
+      .stuco-member {
+          margin: 15px 0;
+          padding: 10px;
+          background: #efefef;
+          border-radius: 10px;
+          text-align: center;
+      }
+      .name {
+          font-size: 18px;
+          font-weight: bold;
+      }
+      .form {
+          font-size:12px;
+      }
+      .stuco-desc {
+          margin: 5px 0;
+      }
+      @media only screen and (min-width: 640px) {
+          .container-fluid {
+              padding:0 100px;
+          }
+      }
+      .abb {
+          color:red;
+          font-size: 14px;
+      }
   </style>
+    <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
 </head>
 
 <body>
+<br>
   <div class="container-fluid">
-    <div class="container">
-        <br><br>
-    <h1>Saint Louis Priory School Student Council Website</h1>
-        <hr>
-        <p><a href="/tasks">Tasks</a> and the <a href="/blog">Announcements</a> are complete.</p>
-        <p>The <a href="/suggestions">Suggestions</a> page looks complete but has no working functions.</p>
-        <p>Whoever originally designed this page commited a grave sin.</p>
-        <h2>Priory Student Council</h2>
-        <p id="welcome">Welcome to the official Priory STUCO website! As your representatives, we always try to listen to your suggestions and come up with new ideas to make highschool as fun as possible. To help us do this, go ahead and leave a suggestion; or if you want you can check out the Announcements page to see what new events are coming up.</p>
-        <h4>Current STUCO Representatives:</h4>
-        <ul>
-        <li>
-            <h6>Form VI</h6>
-            <div class="table">
-                <ul id="horizontal-list">
-                <li><img src="<?php echo url();?>Library/StucoMembers/Preston.png"></li>
-                <li><img src="<?php echo url();?>Library/StucoMembers/Matthew.png"></li>
-                <li><img src="<?php echo url();?>Library/StucoMembers/Louis.png"></li>
-                <li><img src="<?php echo url();?>Library/StucoMembers/Jack.png"></li>
-                <li><img src="<?php echo url();?>Library/StucoMembers/Anthony.png"></li>
-                </ul>
-            </div>
-        </li>
-        <li>Form V</li>
-        <li>Form IV</li>
-        <li>Form III</li>
-        </ul>
+    <h1>Saint Louis Priory School Student Council Website<span class="abb"> (SLPSSCW)</span></h1>
+    <hr>
+    <h2>Coding Club Ramblings™</h2>
+    <p><a href="/tasks">Tasks</a> and the <a href="/blog">Announcements</a> are complete.</p>
+    <p>The <a href="/suggestions">Suggestions</a> page looks complete but has no working functions.</p>
+    <p>Whoever originally designed this page commited a grave sin.</p>
+    <h2>Priory Student Council</h2>
+    <p id="welcome">Welcome to the official Priory STUCO website! As your representatives, we always try to listen to your suggestions and come up with new ideas to make highschool as fun as possible. To help us do this, go ahead and leave a suggestion; or if you want you can check out the Announcements page to see what new events are coming up.</p>
+    <h4>Current STUCO Representatives:</h4>
+    <div class="grid">
+        <div class="grid-item stuco-member">
+            <img src="<?php echo url();?>Library/StucoMembers/Preston.png">
+            <hr>
+            <p class="stuco-desc"><span class="name">Preston</span> <span class="form">Form VI</span></p>
+        </div>
+        <div class="grid-item stuco-member">
+            <img src="<?php echo url();?>Library/StucoMembers/Matthew.png">
+            <hr>
+            <p class="stuco-desc"><span class="name">Matthew</span> <span class="form">Form VI</span></p>
+        </div>
+        <div class="grid-item stuco-member">
+            <img src="<?php echo url();?>Library/StucoMembers/Louis.png">
+            <hr>
+            <p class="stuco-desc"><span class="name">Louis</span> <span class="form">Form VI</span></p>
+        </div>
+        <div class="grid-item stuco-member">
+            <img src="<?php echo url();?>Library/StucoMembers/Jack.png">
+            <hr>
+            <p class="stuco-desc"><span class="name">Jack</span> <span class="form">Form VI</span></p>
+        </div>
+        <div class="grid-item stuco-member">
+            <img src="<?php echo url();?>Library/StucoMembers/Anthony.png">
+            <hr>
+            <p class="stuco-desc"><span class="name">Anthony</span> <span class="form">Form VI</span></p>
+        </div>
     </div>
 </div>
+<script>
+    $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: 250
+    });
+</script>
 </body>
