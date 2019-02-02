b94aa69e42830bf59d62b80f3f6d1818d1cd9e9c
Hexbugman213 <minecrafthex213@gmail.com>
2019-02-02 14:43:25 -0600

    fixed index

--- Views/index.php
+++ Views/index.php
@@ -1,3 +1,25 @@
+<?php
+// base directory
+$base_dir = __DIR__;
+
+// server protocol
+$protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+
+// domain name
+$domain = $_SERVER['SERVER_NAME'];
+
+// base url
+$base_url = preg_replace("!^${doc_root}!", '', $base_dir);
+
+// server port
+$port = $_SERVER['SERVER_PORT'];
+$disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
+
+// put em all together to get the complete base URL
+$url = "${protocol}://${domain}${disp_port}${base_url}";
+
+echo $url; // = http://example.com/path/directory
+?>
 <head>
   <style>
   h1 {
@@ -13,38 +35,26 @@
   <div class="container-fluid">
     <div class="container">
         <br><br>
-    <h1>Saint Louis Priory School Student Council Website <br></h1>
+    <h1>Saint Louis Priory School Student Council Website</h1>
         <hr>
-        <a href="/tasks">Tasks</a> and the <a href="/blog">Announcements</a> are complete.
-        <br> The <a href="/suggestions">Suggestions</a> page looks complete but has no working functions.
-    </div>
-    <br>
-    <h2>Priory Student Council</h2>
-      <p id="welcome">Welcome to the official Priory STUCO website!
-        <br>As your representatives, we always try to
-        <br> listen to your suggestions and come up with new
-      <br>ideas to make highschool as fun as possible.
-      <br> To help us do this, go ahead and leave a suggestion;
-      <br> or if you want you can check out the Announcements
-      <br> page to see what new events are coming up.
-     </p>
-    <div class="container">
+        <p><a href="/tasks">Tasks</a> and the <a href="/blog">Announcements</a> are complete.</p>
+        <p>The <a href="/suggestions">Suggestions</a> page looks complete but has no working functions.</p>
+        <p>Whoever originally designed this page commited a grave sin.</p>
+        <h2>Priory Student Council</h2>
+        <p id="welcome">Welcome to the official Priory STUCO website! As your representatives, we always try to listen to your suggestions and come up with new ideas to make highschool as fun as possible. To help us do this, go ahead and leave a suggestion; or if you want you can check out the Announcements page to see what new events are coming up.</p>
         <h4>Current STUCO Representatives:</h4>
-
         <ul>
         <li>
             <h6>Form VI</h6>
             <div class="table">
-            <ul id="horizontal-list">
-            <li><img src="<?php echo __URL?>Library/StucoMembers/Preston.png"></li>
-            <li><img src="<?php echo __URL?>Library/StucoMembers/Matthew.png"></li>
-            <li><img src="<?php echo __URL?>Library/StucoMembers/Louis.png"></li>
-            <li><img src="<?php echo __URL?>Library/StucoMembers/Jack.png"></li>
-            <li><img src="<?php echo __URL?>Library/StucoMembers/Anthony.png"></li>
-            </ul>
-        </div>
-
-
+                <ul id="horizontal-list">
+                <li><img src="<?php echo $url?>Library/StucoMembers/Preston.png"></li>
+                <li><img src="<?php echo $url?>Library/StucoMembers/Matthew.png"></li>
+                <li><img src="<?php echo $url?>Library/StucoMembers/Louis.png"></li>
+                <li><img src="<?php echo $url?>Library/StucoMembers/Jack.png"></li>
+                <li><img src="<?php echo $url?>Library/StucoMembers/Anthony.png"></li>
+                </ul>
+            </div>
         </li>
         <li>Form V</li>
         <li>Form IV</li>
