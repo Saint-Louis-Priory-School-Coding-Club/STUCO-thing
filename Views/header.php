44b29f58f82869aee41c8249b26651caf06c307a
Hexbugman213 <minecrafthex213@gmail.com>
2019-02-02 15:12:11 -0600

    WHO PUT THIS IN A BOOTSTRAP CONTAINER BOX ?!?!?!?!?!?!?

--- Views/header.php
+++ Views/header.php
@@ -19,7 +19,6 @@ if (isset($_GET['logout'])) {
 }
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
     <a class="navbar-brand" href="/">Priory STUCO</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -81,5 +80,4 @@ if (isset($_GET['logout'])) {
         }
       ?>
     </div>
-      </div>
   </nav>
